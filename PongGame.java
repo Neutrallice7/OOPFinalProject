a7df5cda861a8dc71eae3d3e5302fa6d81e6e4d6
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2022-06-19 15:45:48 +0700

    Update

--- PongGame.java
+++ PongGame.java
@@ -2,11 +2,13 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
-
+//executable
 public class PongGame {
     
     public static void main(String[] args){
+        
         GameFrame frame = new GameFrame();
+
     }
 
 }
